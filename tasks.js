15c5dc4b5fb6b775bb8f2d8622b26f2f07fe1630
Christopher <mwanikigachina@gmail.com>
2024-01-11 18:02:27 +0300

    updated task.js

--- tasks.js
+++ tasks.js
@@ -26,5 +26,47 @@ const divElement = document.getElementById('div');
         }
     }
 
-    divElement.innerHTML = createCounter(5).increment();
+    //divElement.innerHTML = createCounter(5).increment();
+}
+
+
+
+
+{
+    /* Alternative to Array.Map */
+    let map = function (arr, fn){
+        let arr2 = [];
+        for (let i = 0; i < arr.length; i++){
+            arr2[i] = fn(arr[i], i);
+        }
+        return arr2;
+    }
+
+    let arr =[1,2,3]
+    let result = map(arr, function(item, index){
+        return item*2;
+    });
+    //divElement.innerHTML = result;
+
+}
+
+
+
+{
+    /* Alternative to Array.Filter */
+    let filter = function(arr, fn){
+        let filteredArr = [];
+        for (let i = 0; i < arr.length; i++){
+            if (fn(arr[i], i)){
+                filteredArr.push(arr[i]);
+            }
+        }
+        return filteredArr;
+    }
+
+    let arr = [1, 2, 3];
+    let result= filter(arr, function(item, index){
+        return item = item > 2;
+    });
+    divElement.innerHTML = result;
 }
