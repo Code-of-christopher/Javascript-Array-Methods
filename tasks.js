59ba0f7cbec3803e2483fc4bfa1909dda7436a85
Christopher <mwanikigachina@gmail.com>
2024-01-12 12:46:51 +0300

    updated tasks.js

--- tasks.js
+++ tasks.js
@@ -68,5 +68,63 @@ const divElement = document.getElementById('div');
     let result= filter(arr, function(item, index){
         return item = item > 2;
     });
+    //divElement.innerHTML = result;
+}
+
+
+
+
+{
+    /* Alternative to Array.reduce */
+    let reduce = function(nums, fn, init){
+        let final = init;
+        for(let i=0; i<nums.length; i++){
+            final = fn(final, nums[i]);
+        }
+        return final;
+    }
+
+    let nums=[1,2,3];
+    let result = reduce(nums, function(item, init){
+        return item+init;
+    },0);
+    //divElement.innerHTML = result;
+}
+
+
+
+
+{
+    /* Function Composition */
+    let compose = function(functions){
+        return function(x){
+            for (let i=functions.length-1; i>=0; i--){
+                x = functions[i](x);
+            }
+            return x;
+        }
+    }
+
+    const add2 = x => x+2;
+    const times4 = x => x*4;
+    const minus3 = x => x-3;
+
+    const fnArr = [add2, times4, minus3]
+
+    const composedFunction = compose(fnArr);
+
+    const result = composedFunction(40);
+    //divElement.innerHTML = result;
+}
+
+
+
+{
+    /* Return length of arguments passed */
+    function argsLength(...args){
+        return args.length;
+    }
+
+    let result = argsLength('a','b','c','d','e','f');
     divElement.innerHTML = result;
 }
